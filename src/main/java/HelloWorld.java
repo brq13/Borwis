fe6a06ecccbd71735bb5675d5dd5aca3d80c397e
Borwis <dubovbe@yandex.ru>
2020-06-22 00:18:58 +0300

    Bowris test

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven World");
+        Boris boris = new Boris();
+        boris.name = "Boris";
+    }
+}
+
+class Boris {
+    String name;
+
+}
